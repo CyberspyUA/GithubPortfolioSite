a51179c418f476f9ef4849df38f44a5f01ebf966
Nikita Struk <88832410+CyberspyUA@users.noreply.github.com>
2023-11-13 09:25:22 +0000

    Home image updated

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -20,7 +20,7 @@ import PropTypes from "prop-types";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/coding_screen.jpg";
+import image from "../images/home-image.jpg";
 
 const imageAltText = "A load of terms related to game development on black background.";
 
